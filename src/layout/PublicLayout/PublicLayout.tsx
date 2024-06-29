06e2b447d119aca8e2916f8eb7c6cd6f1d9001e6
Дмитрий Куликов <dkulikov@studio-tg.ru>
2024-06-29 11:21:31 +0300

    fix

--- src/layout/PublicLayout/PublicLayout.tsx
+++ src/layout/PublicLayout/PublicLayout.tsx
@@ -2,9 +2,6 @@ import { Outlet } from "react-router-dom";
 import { Footer, Header } from "@/layout";
 
 const PublicLayout = () => {
-  if (true) {
-    throw new Error();
-  }
   return (
     <div className="d-flex flex-column min-vh-100">
       <Header />
